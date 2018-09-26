016e710005e821d8c05778f846931dda28c4024c
AmanGhotra19 <38792080+AmanGhotra19@users.noreply.github.com>
2018-09-26 18:32:09 +0200

    Update suma.java
    demar tercer numero

--- suma.java
+++ suma.java
@@ -1,6 +1,8 @@
 
 int numero1 = 0;
 int numero2 = 0;
+int numero3 = 0;
+
 
 
 
@@ -9,4 +11,8 @@ numero1 = reader.nextInt();
 
 System.out.println("Enter the second number:");
 numero2 = reader.nextInt();
+
+System.out.println("Enter the third number:");
+numero2 = reader.nextInt();
+
 
