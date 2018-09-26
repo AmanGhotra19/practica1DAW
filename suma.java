64e7b552f2d1be3e773d820aa0f5068f975e638e
Aman <aman@leamke.com>
2018-09-26 16:50:03 +0200

    EL arxiu en angles
    El arxiu hem modificat amb angles.

--- suma.java
+++ suma.java
@@ -3,9 +3,9 @@ int numero1 = 0;
 int numero2 = 0;
 
 
-System.out.println("Introduce el primer numero:");
+System.out.println("ENter the first number:");
 numero1 = reader.nextInt();
 
-System.out.println("Introduce el segundo numero:");
+System.out.println("Enter the second number:");
 numero2 = reader.nextInt();
 
